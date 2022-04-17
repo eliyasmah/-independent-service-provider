52c0dac6a99f3fc7a41a129369f6be77d181bf56
eliyasmah <eliyasbubt@gmail.com>
2022-04-17 14:43:01 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,10 +3,10 @@ import CustomLink from '../../CustomeLink/CustomeLink';
 
 const Header = () => {
     return (
-        <div>
+        <div className='text-xl flex gap-4 justify-center text-center bg-slate-300 py-5'>
            <CustomLink to="/">HOME</CustomLink>
            <CustomLink to="/">HOME</CustomLink>
-           <CustomLink to="/blogs">Blogs</CustomLink>
+           <CustomLink to="/blogs">BLOGS</CustomLink>
         </div>
     );
 };
