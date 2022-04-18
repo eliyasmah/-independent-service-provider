2ad8ba7410e58ad453caadd6c4c18c5f2381129e
eliyasmah <eliyasbubt@gmail.com>
2022-04-19 02:18:13 +0600

    about me section done

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './AboutMe.css';
 
 const AboutMe = () => {
     return (
-        <div>
-            <h1>About me section</h1>
+        <div className='about-me'>
+            <div className='aboutme-img mb-3'>
+            <img className='' src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1650311659~exp=1650312259~hmac=2095df7ff2eeca30da94c37fada6975352beea907a46afd4dbedbfc1839ec0d1&w=740" alt="" />
+            </div>
+            <h2>Md. Eliyas</h2>
+            <div className='m-5'> 
+            <p> My name is Md. Eliyas . I am from Bangladesh. I love programming. In future I want to be a full stack web-developer. I already create some project with react.js. Now i am learning from Programming Hero - 'Complete Web Development Course With Jhankar Mahbub ' Web Course . In next 1-2 years, I want to be a full stack web-developer.</p>
+            </div>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,17 +4,28 @@ const Blogs = () => {
     return (
         <div>
             <div className='m-5'>
-                <div className='bg-primary p-5 rounded mb-2 shadow-lg'>
-                    <h1 className='text-xl mb-2 font-bold'>1. Difference between authorization and authentication.</h1>
-                    <p className='text-white'><span className='text-'>Answer:</span> Authentication is a process of verifying who someone is,whereas authorization is a process of verifying what specific applications, files, and data a user has access to.
+                <div className='bg-primary p-3 rounded mb-3 shadow-lg'>
+                    <h3 className='text-xl mb-2 font-bold'>1. Difference between authorization and authentication.</h3>
+                    <p className='text-white'><span className='text-black'>Answer:</span> Authentication is a process of verifying who someone is,whereas authorization is a process of verifying what specific applications, files, and data a user has access to.
                     Authentication work through passwords, one-time pins, biometric information, and other information provided or entered by the user, Authorization work through settings that are implemented and maintained by the organization.
                     Authentication is the first step of good identity and access management process, Authorization always take place after authentication.
                     Authentication visible to and partially changeable by the user, Authorization isn’t visible to or changeable by the user.
                     </p>
                 </div>
-                <div className='bg-slate-200 p-5 rounded-lg shadow-lg'>
-                    <h1 className='text-xl mb-2 font-bold'>2. Why are you using firebase? What other options do you have to implement authentication?</h1>
-                    <p><span className='text-xl text-red-600'>Answer:</span> Semantic tags clearly define the purpose of HTML element. Semantic tags also describes the type of content that the element should contain. Semantic tags give more hooks for styling your content. We can do all of these things easily by using semantic tags and smartly applied CSS. Semantic HTML tags that introduce meaning to the web page rather than just presentation. Examples of semantic HTML tags:h1-h6,blockquote,code,p,ul etc.</p>
+                <div className='bg-primary p-3 rounded mb-3 shadow-lg'>
+                    <h3 className=''>2. Why are you using firebase? What other options do you have to implement authentication?</h3>
+                    <p className='text-white'><span className='text-black'>Answer:</span> The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Other option for Authentication Password-based authentication. Passwords are the most common methods of authentication. Certificate-based authentication. Biometric authentication. Token-based authentication.</p>
+                </div>
+                <div className='bg-primary p-3 rounded mb-2 shadow-lg'>
+                    <h3 className=''>3. What other services does firebase provide other than authentication ?</h3>
+                    <p className='text-white'><span className='text-black'>Answer:</span> 
+                        <ol>
+                            <li>Cloud Messaging</li>
+                            <li>Realtime Database.</li>
+                            <li>Cloud Storage.</li>
+                            <li> Google Analytics.</li>
+                        </ol>
+                    </p>
                 </div>
             </div>
         </div>
