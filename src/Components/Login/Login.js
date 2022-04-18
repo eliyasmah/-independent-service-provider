a8b6ec46c82cc9fd577d866d7b8d097b35b2c269
eliyasmah <eliyasbubt@gmail.com>
2022-04-18 19:40:38 +0600

    link register in firebase

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,17 +1,30 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+      if(user){
+        navigate('/home');
+      }
+
     const handleSubmit = event => {
       event.preventDefault();
       const email = emailRef.current.value;
       const password = passwordRef.current.value;
-      console.log(email, password);
+      signInWithEmailAndPassword(email, password);
     }
 
     const navigateRegister = event => {
@@ -40,7 +53,7 @@ const Login = () => {
                 Submit
             </Button>
         </Form>
-        <p>New to Genius Car? <Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link> </p>
+        <p>New Member? <Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link> </p>
     </div>
     );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Register.css';
+import auth from '../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+    const navigateLogin = () =>{
+        navigate('/login');
+    }
+
+    if(user){
+        navigate('/home');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
+        
+    }
     return (
-        <div className='register-form'>
+        <div className='register-form mt-4'>
         <h2 style={{textAlign: 'center'}}>Please Register</h2>
-        <form >
-            <input type="text" name="name" id="" placeholder='Your Name'/>
+        <form onSubmit={handleRegister} >
+            <input className='rounded' type="text" name="name" id="" placeholder='Your Name'/>
             
-            <input type="email" name="email" id="" placeholder='Email Address' required/>
+            <input className='rounded' type="email" name="email" id="" placeholder='Email Address' required/>
             
-            <input type="password" name="password" id="" placeholder='Password' required/>
-            <input type="submit" value="Register" />
+            <input className='rounded' type="password" name="password" id="" placeholder='Password' required/>
+            <input className='rounded' type="submit" value="Register" />
         </form>
-        {/* <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p> */}
+        <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
     </div>
     );
 };
